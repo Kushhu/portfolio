5214cc9ecac7107f1c4d5b8d39cef0e0cf274e42
Ramni Bhatt <kushagragangwal@gmail.com>
2026-01-23 14:50:33 +0530

    chore: ui updated

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(fixture.componentInstance.title());
+    expect(compiled.querySelector('h3')?.textContent).toContain(fixture.componentInstance.title());
   });
 });
