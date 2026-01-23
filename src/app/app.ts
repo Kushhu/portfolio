d81d34d33e313d315a3cb8b35d4dc848403909ef
Ramni Bhatt <kushagragangwal@gmail.com>
2026-01-23 11:47:50 +0530

    chore: ui ci changed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Ramni');
+  public readonly title = signal('Kushagra Gangwal');
 }
