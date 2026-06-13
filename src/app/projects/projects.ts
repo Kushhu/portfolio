118b396dd536c332a4259847719b25e2295e1486
Ramni Bhatt <kushagragangwal@gmail.com>
2026-06-13 22:46:19 +0530

    add[projects]: added project content with stylings

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -6,6 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
-export class Projects {
+export class Projects implements OnInit, AfterViewInit {
 
+  sections!: any
+  activeFragment = signal<string | null>(null);
+
+  ngOnInit(): void {
+    this.sections = document.querySelectorAll('section');
+  }
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const id = entry.target.id;
+          this.activeFragment.set(id);
+        }
+      });
+    }, { threshold: 0.8 });
+
+    this.sections.forEach((section: any) => observer.observe(section));
+  }
+
+  show(id: string) {
+    document.getElementById(id)?.scrollIntoView();    
+  }
 }
